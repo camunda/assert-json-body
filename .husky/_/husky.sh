a72ad0d30b026fe8fe6ced45d551e5f65fe1f031
Josh Wulf <josh@magikcraft.io>
2026-03-08 11:44:11 +1300

    ci: update package-lock.json

--- .husky/_/husky.sh
+++ .husky/_/husky.sh
@@ -1,26 +1,9 @@
+echo "husky - DEPRECATED
+
+Please remove the following two lines from $0:
+
 #!/usr/bin/env sh
-if [ -z "$husky_skip_init" ]; then
-  debug () {
-    [ "$HUSKY_DEBUG" = "1" ] && echo "husky (debug) - $1"
-  }
-  readonly hook_name="$(basename "$0")"
-  debug "starting $hook_name..."
-  if [ "$HUSKY" = "0" ]; then
-    debug "HUSKY env variable is set to 0, skipping hook"
-    exit 0
-  fi
-  if [ ! -f package.json ]; then
-    debug "package.json not found, skipping hook"
-    exit 0
-  fi
-  command -v node >/dev/null 2>&1 || { debug "node not found, skipping hook"; exit 0; }
-  if [ -f .huskyrc ]; then
-    debug ".huskyrc detected, source it"
-    . .huskyrc
-  fi
-  export readonly husky_skip_init=1
-  sh -e "$0" "$@"
-  exitCode="$?"
-  unset husky_skip_init
-  exit "$exitCode"
-fi
+. \"\$(dirname -- \"\$0\")/_/husky.sh\"
+
+They WILL FAIL in v10.0.0
+"
