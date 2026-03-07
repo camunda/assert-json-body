32d77e4a0f76110d08b5de8c1613832adbc025b5
Josh Wulf <josh@magikcraft.io>
2026-03-08 12:32:59 +1300

    ci: relax commit length rule

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,4 +1,9 @@
 // eslint-disable-next-line no-undef
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    // semantic-release generates changelog bodies with long GitHub URLs
+    'body-max-line-length': [0, 'always'],
+    'footer-max-line-length': [0, 'always'],
+  },
 };
